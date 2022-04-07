60d33e997913c8495c60f2a5f1ed9e93de3237a6
blanche37 <wkddbsghk16@gmail.com>
2022-04-07 23:37:22 +0900

    Initial Commit

---
+++ JuiceMakerApp/JuiceMakerApp/JuiceMakerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceMakerAppApp.swift
+//  JuiceMakerApp
+//
+//  Created by yun on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct JuiceMakerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
