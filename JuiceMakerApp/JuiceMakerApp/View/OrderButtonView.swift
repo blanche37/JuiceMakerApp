8a5f4bc2cd9f0d085d5f4eed2d94ca48c51fb4f5
blanche37 <wkddbsghk16@gmail.com>
2022-04-12 21:48:13 +0900

    feat: OrderButtonView 생성

---
+++ JuiceMakerApp/JuiceMakerApp/View/OrderButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderButtonView.swift
+//  JuiceMakerApp
+//
+//  Created by yun on 2022/04/12.
+//
+
+import SwiftUI
+
+struct OrderButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderButtonView()
+    }
+}
