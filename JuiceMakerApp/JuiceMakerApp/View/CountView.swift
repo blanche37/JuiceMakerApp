199b5cb94ace6b753278695ce751f8eff82ceff3
blanche37 <wkddbsghk16@gmail.com>
2022-04-12 21:41:32 +0900

    feat: CountView구현

---
+++ JuiceMakerApp/JuiceMakerApp/View/CountView.swift
@@ -0,0 +1,23 @@
+//
+//  CountView.swift
+//  JuiceMakerApp
+//
+//  Created by yun on 2022/04/12.
+//
+
+import SwiftUI
+
+struct CountView: View {
+    @Binding var dict: [Fruit: Int]
+    
+    let fruit: Fruit
+    
+    var body: some View {
+        ZStack {
+            Color.gray
+            Text("\(dict[fruit]!)")
+        }
+        .frame(width: 100, height: 30)
+        .cornerRadius(10)
+    }
+}
