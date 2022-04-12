51a92e08fe981e3ea910909654fa50f555b4819d
blanche37 <wkddbsghk16@gmail.com>
2022-04-12 21:29:11 +0900

    feat: FruitView 생성

---
+++ JuiceMakerApp/JuiceMakerApp/View/FruitView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitView.swift
+//  JuiceMakerApp
+//
+//  Created by yun on 2022/04/12.
+//
+
+import SwiftUI
+
+struct FruitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitView()
+    }
+}
