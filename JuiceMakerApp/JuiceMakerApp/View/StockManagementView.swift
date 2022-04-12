723d11efca7ea52091912fcfc1135c8a394b0944
blanche37 <wkddbsghk16@gmail.com>
2022-04-12 22:13:42 +0900

    feat: StockManagementView생성

---
+++ JuiceMakerApp/JuiceMakerApp/View/StockManagementView.swift
@@ -0,0 +1,20 @@
+//
+//  StockManagementView.swift
+//  JuiceMakerApp
+//
+//  Created by yun on 2022/04/12.
+//
+
+import SwiftUI
+
+struct StockManagementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StockManagementView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockManagementView()
+    }
+}
