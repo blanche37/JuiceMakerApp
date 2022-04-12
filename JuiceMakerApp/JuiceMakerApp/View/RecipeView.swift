9612c1404e985b8090b41ed051d44e3596a6d889
blanche37 <wkddbsghk16@gmail.com>
2022-04-13 03:46:04 +0900

    feat: RecipeView 생성

---
+++ JuiceMakerApp/JuiceMakerApp/View/RecipeView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeView.swift
+//  JuiceMakerApp
+//
+//  Created by yun on 2022/04/13.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeView()
+    }
+}
