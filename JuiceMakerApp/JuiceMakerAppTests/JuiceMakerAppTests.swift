1f385c31fe047e314b864edcf21893821ce2e171
blanche37 <wkddbsghk16@gmail.com>
2022-04-13 19:52:06 +0900

    Add: UnitTest file 추가

---
+++ JuiceMakerApp/JuiceMakerAppTests/JuiceMakerAppTests.swift
@@ -0,0 +1,13 @@
+//
+//  JuiceMakerAppTests.swift
+//  JuiceMakerAppTests
+//
+//  Created by yun on 2022/04/13.
+//
+
+import XCTest
+@testable import JuiceMakerApp
+
+class JuiceMakerAppTests: XCTestCase {
+
+}
