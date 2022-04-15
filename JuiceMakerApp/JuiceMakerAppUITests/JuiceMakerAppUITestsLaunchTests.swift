44f1551bd468409cb170b34d7aa9a7cda6b4403c
blanche37 <wkddbsghk16@gmail.com>
2022-04-15 23:18:37 +0900

    Add: Add UITest Bundle

---
+++ JuiceMakerApp/JuiceMakerAppUITests/JuiceMakerAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JuiceMakerAppUITestsLaunchTests.swift
+//  JuiceMakerAppUITests
+//
+//  Created by yun on 2022/04/15.
+//
+
+import XCTest
+
+class JuiceMakerAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
